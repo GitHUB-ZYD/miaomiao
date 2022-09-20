e2cdc8b5ac89a293e8ef24d83ea6998c70102f69
zyd <2720725749@qq.com>
2022-09-20 17:52:18 +0800

    add createComponent

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,27 @@
+<template>
+   <div id="main">
+    <Header title="我的喵喵"></Header>
+    <div id="content">
+      <Login></Login>
+      <Tabbar></Tabbar>
+    </div>
+  </div>
+</template>
+
+<script>
+    import Header from "@/components/header"
+    import Tabbar from "@/components/tabbar"
+    import Login from '@/components/login'
+    export default {
+        name:"mine",
+        components:{
+            Header,
+            Tabbar,
+            Login
+        }
+    }
+</script>
+
+<style>
+
+</style>
